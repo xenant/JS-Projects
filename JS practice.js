1668beb70c0e0d7f88918103c9dfcff130923570
xenant <xenant.inc@gmail.com>
2022-07-27 10:38:25 -0700

    JS practice problems

--- JS practice.js
+++ JS practice.js
@@ -1,1 +1,40 @@
-//using this file to start practicing some JS in case there is a coding portion of the interview
+// Write a function that takes an object (a) and a string (b) as argument
+// Return true if the object has a property with key 'b'
+// Return false otherwise
+// Tipp: test case 3 is a bit tricky because the value of property 'z' is undefined
+// But the property itself exists
+// function myFunction(a, b) {
+//     return b in a;
+//  }
+
+
+
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+// function myFunction(a, n)
+// {
+// return a[n-1]
+// }
+
+
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key country
+//myFunction({ continent: 'Asia', country: 'Japan' }) ------ > Expected 'Japan'
+// function
+// myFunction(obj){
+// return obj.country 
+// }
+
+
+
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+// function
+// myFunction(a, b)
+// {
+// x = typeof(a) == typeof(b);
+// y = a == b;
+// return x && y
+// }
+// or you can use --> a === b (strict equality comparison operator in JavaScript, which returns false for the values which are not of a similar type.)
+
