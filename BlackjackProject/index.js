bcf08ae68cd179e7bf6151586bfdadba8c257545
xenant <xenant.inc@gmail.com>
2022-08-14 17:44:03 -0700

    Making Progress, on 3:18:11

--- BlackjackProject/index.js
+++ BlackjackProject/index.js
@@ -1,6 +1,6 @@
 let firstCard = 2;
 let secondCard = 5;
-
+let cards = [firstCard,secondCard]; //ordered list of items 
 let sum = firstCard + secondCard;
 let wonBJ = false;
 let isAlive = true;
@@ -8,8 +8,21 @@ let message = "";
 let messageEl = document.getElementById("message-el")
 let sumElement = document.querySelector("#sumElement")
 let cardsEl = document.querySelector("#cards-el")
+
 function startGame(){
-    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard
+    renderGame();
+}
+
+
+function renderGame(){
+    //  render out firstCard and secondCard
+    cardsEl.textContent = "Cards: "
+    for( let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " ";
+    }
+
+
+    // render out ALL the cards we have 
     sumElement.textContent = "Sum: " + sum
     if (sum <= 20){
         message ="Wanna draw another? lol"
@@ -20,9 +33,14 @@ function startGame(){
         message ="LMAO you lost get guud"
         isAlive = false; 
     }
-    messageEl.textContent = message 
+    messageEl.textContent = message   
 }
 
 function newCard(){
     console.log("Getting a new card from the deck")
+
+    let nextCard = 6;
+    sum += nextCard;
+    cards.push(nextCard);
+    renderGame();
 }
---
+++ BlackjackProject/scratch.js
@@ -0,0 +1,13 @@
+let timeOne = 111
+
+let timeTwo = 144
+
+function totalRaceTime() {
+   
+
+    return  timeOne + timeTwo;
+}
+
+let gamers = totalRaceTime();
+
+console.log(gamers)
